9a608656ce2e88ab45cc43b0e4ae36c05e881bfa
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-10-06 21:02:27 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,51 @@
+let iterator = 0.12;
+
+let scl = 20;
+let cols;
+let rows;
+
+let zoff = 0;
+
 function preload(){
   // put preload code here
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+
+  let cnv = createCanvas(800, 800);
+  let xCnv = (windowWidth - width) / 2;
+  let yCnv = (windowHeight - height) / 2;
+  cnv.position(xCnv, yCnv);
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+
+  let yoff = 0;
+  for (let x = 0; x < cols; x++){
+    let xoff = 0;
+    for (let y = 0; y < rows; y ++){
+      let index = ( x + y  * width) * 4;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let v = p5.Vector.fromAngle(angle);
+      xoff += iterator;
+      stroke(0);
+      fill(lerpColor(color("sandyBrown"), color("PowderBlue"),yoff / 5));
+
+      push();
+      noStroke();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      rect(0, 0, scl / 2 , scl * 2, 10);
+      pop();
+    }
+    yoff += iterator;
+
+    zoff += 0.0005;
+  }
+
+
 }
