97a24f452a066333ce781e9ae769c704b24f2a29
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-10-12 16:39:05 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,10 +23,17 @@ function setup() {
 
 function draw() {
   background(255);
+  if (mouseIsPressed==true){
+    Dynamic();
+  } else {
+    Static();
+  }
+}
 
+function Dynamic() {
   let yoff = 0;
   for (let x = 0; x < cols; x++){
-    let xoff = 0;
+    let xoff = mouseX / 500;
     for (let y = 0; y < rows; y ++){
       let index = ( x + y  * width) * 4;
       let angle = noise(xoff, yoff, zoff) * TWO_PI;
@@ -47,6 +54,40 @@ function draw() {
 
     zoff += 0.0005;
   }
+}
 
+function Static() {
+
+  let yoff = 0;
+  for (let x = 0; x < cols; x++){
+    let xoff = 0;
+    for (let y = 0; y < rows; y ++){
+      let index = ( x + y  * width) * 4;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let v = p5.Vector.fromAngle(angle);
+
+
+      stroke(0);
+      fill(0);
+
+      push();
+      noStroke();
+      translate(x * scl, y * scl);
+      rect(0, 0, scl / 2);
+      pop();
+    }
+  }
+  push();
+  fill(255);
+  noStroke();
+  rectMode(CENTER);
+  rect(width/2, height/4, 400,scl*3);
+  mouseval = false;
+  textSize(24);
+  textAlign(CENTER,CENTER);
+  fill(0);
+  text("Hold down left mouse button",width/2,height/4);
+
+  pop();
 
 }
